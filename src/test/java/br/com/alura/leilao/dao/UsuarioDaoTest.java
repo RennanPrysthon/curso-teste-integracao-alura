1cdd610a8e9cd819958d878e76699f3b55c18db7
Rennan Prysthon <rennandelcastillo@gmail.com>
2021-04-15 11:24:38 -0300

    refact: Refactor some test code

--- src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -2,6 +2,8 @@ package br.com.alura.leilao.dao;
 
 import br.com.alura.leilao.model.Usuario;
 import br.com.alura.leilao.util.JPAUtil;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.persistence.EntityManager;
@@ -11,30 +13,37 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class UsuarioDaoTest {
     private UsuarioDao dao;
+    private EntityManager em;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.em = JPAUtil.getEntityManager();
+        this.dao = new UsuarioDao(em);
+        em.getTransaction().begin();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        em.getTransaction().rollback();
+    }
 
     @Test
     void deveriaEncontrarUsuarioCadastrado() {
-        EntityManager em = JPAUtil.getEntityManager();
-        this.dao = new UsuarioDao(em);
-
-        Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
-        em.getTransaction().begin();
-        em.persist(usuario);
-        em.getTransaction().commit();
+        Usuario usuario =criarUsuario();
         Usuario encontrado = this.dao.buscarPorUsername(usuario.getNome());
-
         assertNotNull(encontrado);
     }
     @Test
     void naoDeveriaEncontrarUsuarioNaoCadastrado() {
-        EntityManager em = JPAUtil.getEntityManager();
-        this.dao = new UsuarioDao(em);
+        criarUsuario();
+        assertThrows(NoResultException.class,
+            () -> this.dao.buscarPorUsername("beltrano")
+        );
+    }
 
+    private Usuario criarUsuario() {
         Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
-        em.getTransaction().begin();
         em.persist(usuario);
-        em.getTransaction().commit();
-
-        assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername("beltrano"));
+        return usuario;
     }
 }
