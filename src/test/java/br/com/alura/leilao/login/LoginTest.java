34c5a00d28d9c106d5704073b160dc06e9ba9771
Rennan Prysthon <rennandelcastillo@gmail.com>
2021-04-15 17:30:45 -0300

    test: should not login with invalid data

--- src/test/java/br/com/alura/leilao/login/LoginTest.java
+++ src/test/java/br/com/alura/leilao/login/LoginTest.java
@@ -1,40 +1,59 @@
 package br.com.alura.leilao.login;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LoginTest {
 
+    public static final String URL_LOGIN = "http://localhost:8080/login";
     private WebDriver browser;
 
-    @BeforeEach
-    public void instantiateBrowser() {
+    @BeforeAll
+    public static void beforeAll() {
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+    }
+
+    @BeforeEach
+    public void beforeEach() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox"); // Bypass OS security model
-
         this.browser = new ChromeDriver(options);
+        browser.navigate().to(URL_LOGIN);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        browser.quit();
     }
 
     @Test
     public void deveriaEfetuarLoginComDadosValidos() {
-        browser.navigate().to("http://localhost:8080/login");
         browser.findElement(By.id("username")).sendKeys("fulano");
         browser.findElement(By.id("password")).sendKeys("pass");
         browser.findElement(By.id("login-form")).submit();
 
-        assertFalse(browser.getCurrentUrl().equals("http://localhost:8080/login"));
+        assertFalse(browser.getCurrentUrl().equals(URL_LOGIN));
         assertEquals("fulano",browser.findElement(By.id("usuario-logado")).getText());
-
-        browser.quit();
     }
 
+    @Test
+    public void naoDeveriaLogarComDadosInvalidos() {
+        browser.findElement(By.id("username")).sendKeys("invalido");
+        browser.findElement(By.id("password")).sendKeys("invalido");
+        browser.findElement(By.id("login-form")).submit();
+
+        assertTrue(browser.getCurrentUrl().contains(URL_LOGIN));
+        assertTrue(browser.getPageSource().contains("Usuário e senha inválidos."));
+        assertThrows(NoSuchElementException.class, () -> browser.findElement(By.id("usuario-logado")).getText());
+    }
 }
 
