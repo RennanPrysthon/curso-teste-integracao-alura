3db6cf9bca9293a8ca7eb52c6796c50ef123a7e2
Rennan Prysthon <rennandelcastillo@gmail.com>
2021-04-15 16:57:05 -0300

    feat: Added Selenium to project, created Hello World test and installed chomedriver

---
+++ src/test/java/br/com/alura/leilao/dao/HelloWorldSelenium.java
@@ -0,0 +1,22 @@
+package br.com.alura.leilao.dao;
+
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class HelloWorldSelenium {
+
+    @Test
+    public void hello() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        WebDriver browser = new ChromeDriver(options);
+        browser.navigate().to("http://localhost:8080/leilao");
+        browser.quit();
+    }
+
+}
