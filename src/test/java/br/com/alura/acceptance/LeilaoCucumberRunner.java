f9ebb67dd4826153242e2078cac363ff84a2928b
Rennan Prysthon <rennandelcastillo@gmail.com>
2021-04-16 22:35:28 -0300

    feat: added cucumber test

---
+++ src/test/java/br/com/alura/acceptance/LeilaoCucumberRunner.java
@@ -0,0 +1,11 @@
+package br.com.alura.acceptance;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:features")
+public class LeilaoCucumberRunner {
+
+}
